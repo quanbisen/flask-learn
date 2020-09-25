e1f0b1a9d33060225ed60decfc4e8d902e9c12ea
lollipop <1769128867@qq.com>
2020-09-25 08:42:28 +0800

    flag:upload file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from html import escape
 
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
 from flask import request
 
@@ -22,6 +23,11 @@ def print_test():
     return render_template('test.html', message='Backend message')
 
 
+@app.route('/directory', methods=['GET'])
+def directory():
+    return render_template('directory/index.html')
+
+
 @app.route('/user/<username>', methods=['GET'])
 def show_user_profile(username):
     return 'User %s' % username
@@ -35,6 +41,45 @@ def show_post_id(post_id):
 @app.route('/path/<path:sub_path>', methods=['GET'])
 def show_sub_path(sub_path):
     return 'Path %s' % sub_path
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/login')
+def login():
+    return '1'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', name='lollipop'))
+#     print(url_for('profile', username='John Doe'))
+
+
+with app.test_request_context('/login', method='GET'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/login'
+    assert request.method == 'GET'
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload-test.html')
+    elif request.method == 'POST':
+        file = request.files['file']
+        file.save('/var/www/test.txt')
+        return 'upload success.'
 
 
 if __name__ == '__main__':
